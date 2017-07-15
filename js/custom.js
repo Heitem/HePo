e50b981b37ec056388b48be4627589ec12cf4dc0
Heitem OULED-LAGHRIYEB <heitem12@hotmail.com>
2017-07-15 12:56:20 +0100

    Add competences Add contact Update font-awesome Update jquery Change footer

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,105 @@ function calculateAge (birthDate, otherDate) {
 
 var t1 = document.querySelector(".lead");
 t1.innerText += " " + calculateAge("07/30/1993", date) + " ans.";
+
+$( document ).ready(function() {
+	$("#android-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 90,
+            iconColor: '#10394B',
+            foregroundColor: '#10394B',
+            fontColor: '#10394B',
+            showPercent: true,
+            text: 'Android',
+            textBelow: true,
+            textColor: '#10394B'
+           });
+   });
+
+   $( document ).ready(function() {
+	$("#ios-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 40,
+            iconColor: '#10394B',
+            foregroundColor: '#10394B',
+            fontColor: '#10394B',
+            showPercent: true,
+            text: 'iOS',
+            textBelow: true,
+            textColor: '#10394B'
+           });
+   });
+
+   $( document ).ready(function() {
+	$("#ionic-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 70,
+            iconColor: '#10394B',
+            foregroundColor: '#10394B',
+            fontColor: '#10394B',
+            showPercent: true,
+            text: 'Ionic Framework',
+            textBelow: true,
+            textColor: '#10394B'
+           });
+   });
+
+   $( document ).ready(function() {
+	$("#cordova-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 60,
+            iconColor: '#10394B',
+            foregroundColor: '#10394B',
+            fontColor: '#10394B',
+            showPercent: true,
+            text: 'Cordova',
+            textBelow: true,
+            textColor: '#10394B'
+           });
+   });
+
+   $( document ).ready(function() {
+	$("#angular-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 40,
+            iconColor: '#10394B',
+            foregroundColor: '#10394B',
+            fontColor: '#10394B',
+            showPercent: true,
+            text: 'Angular 4',
+            textBelow: true,
+            textColor: '#10394B'
+           });
+   });
+
+   $( document ).ready(function() {
+	$("#java-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 80,
+            iconColor: '#10394B',
+            foregroundColor: '#10394B',
+            fontColor: '#10394B',
+            showPercent: true,
+            text: 'Java',
+            textBelow: true,
+            textColor: '#10394B'
+           });
+   });
