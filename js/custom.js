acc18f5c748afe2f835e7bc0768406ed117b52eb
Heitem OULED-LAGHRIYEB <heitem12@hotmail.com>
2017-07-16 19:53:39 +0100

    Change intro Change animations Add diplomes Change colors

--- js/custom.js
+++ js/custom.js
@@ -18,109 +18,9 @@ function calculateAge (birthDate, otherDate) {
 var t1 = document.querySelector(".lead");
 t1.innerText += " " + calculateAge("07/30/1993", date) + " ans.";
 
-$( document ).ready(function() {
-	$("#android-circle").circliful({
-            animation: 1,
-            animationStep: 6,
-            foregroundBorderWidth: 5,
-            backgroundBorderWidth: 1,
-            percent: 90,
-            iconColor: '#10394B',
-            foregroundColor: '#10394B',
-            fontColor: '#10394B',
-            showPercent: true,
-            text: 'Android',
-            textBelow: true,
-            textColor: '#10394B'
-           });
-   });
-
-   $( document ).ready(function() {
-	$("#ios-circle").circliful({
-            animation: 1,
-            animationStep: 6,
-            foregroundBorderWidth: 5,
-            backgroundBorderWidth: 1,
-            percent: 40,
-            iconColor: '#10394B',
-            foregroundColor: '#10394B',
-            fontColor: '#10394B',
-            showPercent: true,
-            text: 'iOS',
-            textBelow: true,
-            textColor: '#10394B'
-           });
-   });
-
-   $( document ).ready(function() {
-	$("#ionic-circle").circliful({
-            animation: 1,
-            animationStep: 6,
-            foregroundBorderWidth: 5,
-            backgroundBorderWidth: 1,
-            percent: 70,
-            iconColor: '#10394B',
-            foregroundColor: '#10394B',
-            fontColor: '#10394B',
-            showPercent: true,
-            text: 'Ionic Framework',
-            textBelow: true,
-            textColor: '#10394B'
-           });
-   });
-
-   $( document ).ready(function() {
-	$("#cordova-circle").circliful({
-            animation: 1,
-            animationStep: 6,
-            foregroundBorderWidth: 5,
-            backgroundBorderWidth: 1,
-            percent: 60,
-            iconColor: '#10394B',
-            foregroundColor: '#10394B',
-            fontColor: '#10394B',
-            showPercent: true,
-            text: 'Cordova',
-            textBelow: true,
-            textColor: '#10394B'
-           });
-   });
-
-   $( document ).ready(function() {
-	$("#angular-circle").circliful({
-            animation: 1,
-            animationStep: 6,
-            foregroundBorderWidth: 5,
-            backgroundBorderWidth: 1,
-            percent: 40,
-            iconColor: '#10394B',
-            foregroundColor: '#10394B',
-            fontColor: '#10394B',
-            showPercent: true,
-            text: 'Angular 4',
-            textBelow: true,
-            textColor: '#10394B'
-           });
-   });
-
-   $( document ).ready(function() {
-	$("#java-circle").circliful({
-            animation: 1,
-            animationStep: 6,
-            foregroundBorderWidth: 5,
-            backgroundBorderWidth: 1,
-            percent: 80,
-            iconColor: '#10394B',
-            foregroundColor: '#10394B',
-            fontColor: '#10394B',
-            showPercent: true,
-            text: 'Java',
-            textBelow: true,
-            textColor: '#10394B'
-           });
-   });
-
    $(document).ready(function(){
+
+    var i = 0;
  
   // hide our element on page load
   $('.text .container').css('opacity', 0);
@@ -128,18 +28,116 @@ $( document ).ready(function() {
   $('.diplomes .container').css('opacity', 0);
   $('.gallery .container').css('opacity', 0);
   $('.contact .container').css('opacity', 0);
-  $('.map').css('opacity', 0);
+//   $('.map').css('opacity', 0);
  
   $('.text .container').waypoint(function() {
       $('.text .container').addClass('fadeInLeft');
   }, { offset: '70%' });
 
   $('.competences .container').waypoint(function() {
-      $('.competences .container').addClass('fadeInRight');
+      $('.competences .container').addClass('fadeIn');
+      
+      if(i < 1){
+          
+      $("#android-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 90,
+            iconColor: '#69C2E4',
+            foregroundColor: '#69C2E4',
+            fontColor: '#69C2E4',
+            showPercent: true,
+            text: 'Android',
+            textBelow: true,
+            textColor: '#69C2E4'
+           });
+
+        $("#ios-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 40,
+            iconColor: '#69C2E4',
+            foregroundColor: '#69C2E4',
+            fontColor: '#69C2E4',
+            showPercent: true,
+            text: 'iOS',
+            textBelow: true,
+            textColor: '#69C2E4'
+           });
+
+           $("#ionic-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 70,
+            iconColor: '#69C2E4',
+            foregroundColor: '#69C2E4',
+            fontColor: '#69C2E4',
+            showPercent: true,
+            text: 'Ionic Framework',
+            textBelow: true,
+            textColor: '#69C2E4'
+           });
+
+           $("#cordova-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 60,
+            iconColor: '#69C2E4',
+            foregroundColor: '#69C2E4',
+            fontColor: '#69C2E4',
+            showPercent: true,
+            text: 'Cordova',
+            textBelow: true,
+            textColor: '#69C2E4'
+           });
+
+           $("#angular-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 40,
+            iconColor: '#69C2E4',
+            foregroundColor: '#69C2E4',
+            fontColor: '#69C2E4',
+            showPercent: true,
+            text: 'Angular 4',
+            textBelow: true,
+            textColor: '#69C2E4'
+           });
+
+           $("#java-circle").circliful({
+            animation: 1,
+            animationStep: 6,
+            foregroundBorderWidth: 5,
+            backgroundBorderWidth: 1,
+            percent: 80,
+            iconColor: '#69C2E4',
+            foregroundColor: '#69C2E4',
+            fontColor: '#69C2E4',
+            showPercent: true,
+            text: 'Java',
+            textBelow: true,
+            textColor: '#69C2E4'
+           });
+
+           i++;
+
+           console.log(i);
+      }
+
   }, { offset: '70%' });
 
   $('.diplomes .container').waypoint(function() {
-      $('.diplomes .container').addClass('fadeInDown');
+      $('.diplomes .container').addClass('fadeInUp');
   }, { offset: '70%' });
 
   $('.gallery .container').waypoint(function() {
@@ -150,9 +148,9 @@ $( document ).ready(function() {
       $('.contact .container').addClass('rollIn');
   }, { offset: '70%' });
 
-  $('.map').waypoint(function() {
-      $('.map').addClass('fadeIn');
-  }, { offset: '70%' });
+//   $('.map').waypoint(function() {
+//       $('.map').addClass('fadeIn');
+//   }, { offset: '70%' });
 
   
 //   $('.competences .container').css('opacity', 1);
